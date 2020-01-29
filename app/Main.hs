40a9ec1ce5643a30b0249df4c5a8c961327aee2b
maxwillf <maxwilliam780@gmail.com>
2020-01-29 17:07:10 -0300

    almost there, need to unwrap maybe

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,43 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.Text.Lazy.Encoding as LTE (decodeUtf8)
 import qualified Data.Text.Lazy.IO as IO (putStrLn,writeFile)
 import qualified Data.Text as T 
+import qualified Data.List as List (find,tails)
 import qualified Data.Text.Lazy as LT (append,pack,unpack)
 import Data.Char (chr)
 
---openURL :: String -> IO String
---openURL x = (map BS.w2c) .  L.unpack <$> simpleHttp x
+
+filterTest :: [(TagName,[Attr])]
+filterTest = [
+  (stringToTagName "div", stringToAttr "class" "article-header"),
+  (stringToTagName "div", stringToAttr "class" "p-main-contents")]
+
+filterTokens :: [(TagName,[Attr])] -> [Token] -> [Token]
+filterTokens tagsTuples tokens = concat nestedList where
+                           nestedList = filterFunction <*> (pure tokens) 
+                           filterFunction = (map (uncurry $ filterToken) tagsTuples)
+
+filterToken :: TagName -> [Attr] -> [Token] -> [Token]
+filterToken tagname attr tokens = maybe filtered ++ [TagClose tagname] where
+                      filtered   = List.find (\x -> (foldr (countToken tagname) 0) == 0) withoutHead 
+                      withoutHead = List.tails (drop 1 dropped)
+                      dropped    = dropWhile ((/=) beginToken) tokens 
+                      beginToken = TagOpen tagname attr
+                      endToken   = TagClose tagname
+
+countToken :: TagName -> Token -> Int -> Int
+countToken tagname (TagOpen  tag _) x 
+  | tag == tagname = x+1
+countToken tagname (TagClose tag ) x 
+  | tag == tagname = x-1
+countToken _ _ x = x
+
+stringToAttr :: String -> String -> [Attr]
+stringToAttr attr values = [Attr (T.pack attr) (T.pack values)]
+
+stringToTagName :: String -> TagName
+stringToTagName str = T.pack str
+
+--tupleToToken :: (TagName,[Attr])
 
 spjPapers :: IO ()
 spjPapers = do
@@ -27,22 +59,12 @@ spjPapers = do
         let body = response ^. responseBody :: L.ByteString
         let tokens = parseTokensLazy decodedBody where
                      decodedBody = LTE.decodeUtf8 body
-        let parsedHtml = LT.append  (renderTokens article) (LT.pack "</article>")
+        let parsedHtml = renderTokens article
                     where 
-                      article = takeWhile((/=) endToken) dropped
-                      dropped = dropWhile ((/=) beginToken) tokens 
-                      beginToken = TagOpen tagName [Attr (T.pack "class") (T.pack "article-content")]
-                      endToken = TagClose tagName
-                      tagName = T.pack "article" :: TagName
+                      article = filterTokens filterTest tokens 
         let unpacked = LT.unpack $ parsedHtml 
-        IO.writeFile "article3.html" parsedHtml
+        --IO.writeFile "article3.html" parsedHtml
         putStrLn unpacked
-        --writeFile "article2.html" article
-        --let links = map f $ sections (~== "<A>") $
-        --            takeWhile (~/= "<a name=haskell>") $
-        --            drop 5 $ dropWhile (~/= "<a name=current>") tags
-        --mapM_ putStrLn links
-        --putStr $ unlines links
 
 main :: IO ()
 main = spjPapers
