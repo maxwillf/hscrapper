18dbc5e8e1d69c96c96546be29b46522dd69b00d
maxwillf <maxwilliam780@gmail.com>
2020-01-31 19:06:05 -0300

    for some reason rendered strings are not being printed outside ghci, perhaps i need to conver to strict text

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Network.Wreq
@@ -7,6 +8,7 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.Text.Lazy.Encoding as LTE (decodeUtf8)
 import qualified Data.Text.Lazy.IO as IO (putStrLn,writeFile)
 import qualified Data.Text as T 
+import qualified Data.Text.IO as TIO (putStrLn) 
 import qualified Data.List as List (find,inits)
 import Data.Maybe (fromJust)
 import qualified Data.Text.Lazy as LT (append,pack,unpack)
@@ -21,28 +23,35 @@ getLinksFromTokens tokens = fromJust <$> (filter (\x -> x /= Nothing)) maybeLink
 
 getLinkFromAnchorToken :: Token -> Maybe T.Text
 getLinkFromAnchorToken (TagOpen tagname attrs)
-  | tagname == T.pack "a" = hrefValue where
+  | tagname == "a" = hrefValue where
                             hrefValue = (\(Attr x y) -> y) <$> hrefAttr
-                            hrefAttr = (List.find (\(Attr y z) -> y == T.pack "href") attrs)
+                            hrefAttr = (List.find (\(Attr y z) -> y == "href") attrs)
 getLinkFromAnchorToken _ = Nothing 
 
 filterAnchors :: [Token] -> [Token]
 filterAnchors tokens = filter isAnchor tokens
 
 isAnchor :: Token -> Bool
-isAnchor (TagOpen tagname attrs) = tagname == T.pack "a"
+isAnchor (TagOpen tagname attrs) = tagname == "a"
 isAnchor _ = False
 
-filterTest :: [(TagName,[Attr])]
-filterTest = [
-  (stringToTagName "div", stringToAttr "class" "article-header"),
-  (stringToTagName "div", stringToAttr "class" "p-main-contents")]
+articlesFilterTags :: [(TagName,[Attr])]
+articlesFilterTags = [
+  ("div", [Attr "class" "article-header"]),
+  ("div", [Attr "class" "p-main-contents"])]
+
+crawlingFilterTags :: [(TagName,[Attr])]
+crawlingFilterTags = [
+  ("li", [Attr "class" "p-list-item  "]),
+  ("li", [Attr "class" "p-list-item p-list-item--small-thumb "])]
 
 filterTokens :: [(TagName,[Attr])] -> [Token] -> [Token]
 filterTokens tagsTuples tokens = concat nestedList where
                            nestedList = filterFunction <*> (pure tokens) 
                            filterFunction = (map (uncurry $ filterToken) tagsTuples)
 
+-- tratar erro aqui, caso a tag nao seja encontrada o programa irá retornar uma seção 
+-- com a função fromJust
 filterToken :: TagName -> [Attr] -> [Token] -> [Token]
 filterToken tagname attr tokens = fromJust (List.find (\x -> mapping x == 0) withoutHead) where
                       mapping   = (foldr (countToken tagname) 0)
@@ -58,28 +67,47 @@ countToken tagname (TagClose tag ) x
   | tag == tagname = x-1
 countToken _ _ x = x
 
-stringToAttr :: String -> String -> [Attr]
-stringToAttr attr values = [Attr (T.pack attr) (T.pack values)]
+responseToTokens :: Response L.ByteString -> [Token]
+responseToTokens response = parsedTokens where
+                  parsedTokens = parseTokensLazy decodedBody
+                  decodedBody = LTE.decodeUtf8 (response ^. responseBody)
 
-stringToTagName :: String -> TagName
-stringToTagName str = T.pack str
-
-spjPapers :: IO ()
-spjPapers = do
-  
-        --let url = "http://research.microsoft.com/en-us/people/simonpj/"
-        let url = "https://www.yomiuri.co.jp/national/20200128-OYT1T50285/"
-        --let url = "https://www.yomiuri.co.jp/world/20200129-OYT1T50107/"
-        --let url = "https://www.yomiuri.co.jp/hobby/atcars/impression/20200123-OYT8T50020/"
-        response <- get url
-        let body = response ^. responseBody :: L.ByteString
-        let tokens = parseTokensLazy decodedBody where
-                     decodedBody = LTE.decodeUtf8 body
-        let parsedHtml = renderTokens article where 
-                      article = filterTokens filterTest tokens 
-        let unpacked = LT.unpack $ parsedHtml 
-        --IO.writeFile "article3.html" parsedHtml
-        putStrLn unpacked
+responseToFilteredTokens ::  [(TagName, [Attr])] -> Response L.ByteString -> [Token]
+responseToFilteredTokens filterTags response  = 
+                  filterTokens filterTags (responseToTokens response)
 
 main :: IO ()
-main = spjPapers
+main = do
+
+        let crawlingPath = "https://www.yomiuri.co.jp/news/" 
+        response <- get crawlingPath
+        let filteredTokens = (responseToFilteredTokens crawlingFilterTags response)
+        let crawlingLinks = getLinksFromTokens filteredTokens
+        mapM_ TIO.putStrLn crawlingLinks
+        -- IO.putStrLn (renderTokens filteredTokens)
+        let responses = get <$> (map T.unpack crawlingLinks)
+        let scrapped = map (fmap (responseToFilteredTokens articlesFilterTags )) responses
+        let rendered = map (fmap (renderTokens)) scrapped
+        IO.putStrLn <$> (head rendered)
+        mapM_ (fmap IO.putStrLn) rendered
+        return ()
+        -- mapM_ IO.putStrLn responses
+        --let url = "http://research.microsoft.com/en-us/people/simonpj/"
+        -- let url = "https://www.yomiuri.co.jp/national/20200128-OYT1T50285/"
+        --let url = "https://www.yomiuri.co.jp/world/20200129-OYT1T50107/"
+        --let url = "https://www.yomiuri.co.jp/hobby/atcars/impression/20200123-OYT8T50020/"
+        -- response <- get url
+        -- let body = response ^. responseBody :: L.ByteString
+        -- let tokens = parseTokensLazy decodedBody where
+        --              decodedBody = LTE.decodeUtf8 body
+        -- let parsedHtml = renderTokens article where 
+        --               article = filterTokens articlesFilterTags tokens 
+        -- let unpacked = LT.unpack $ parsedHtml 
+        -- --IO.writeFile "article3.html" parsedHtml
+        -- putStrLn unpacked
+
+-- stringToAttr :: String -> String -> [Attr]
+-- stringToAttr attr values = [Attr (T.pack attr) (T.pack values)]
+
+-- stringToTagName :: String -> TagName
+-- stringToTagName str = T.pack str
