60a1d06c9ea6540701887dcee5896e2ffeccaace
maxwillf <maxwilliam780@gmail.com>
2020-02-03 16:26:30 -0300

    now properly scrapping all hrefs and pages but still need a way to properly store the results

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,13 @@ import Network.Wreq
 import Control.Lens
 import Text.HTML.Parser
 import qualified Data.ByteString.Lazy as L
-import qualified Data.Text.Lazy.Encoding as LTE (decodeUtf8)
+import qualified Data.Text.Lazy.Encoding as LTE (encodeUtf8,decodeUtf8)
 import qualified Data.Text.Lazy.IO as IO (putStrLn,writeFile)
 import qualified Data.Text as T 
 import qualified Data.Text.IO as TIO (putStrLn) 
-import qualified Data.List as List (find,inits)
+import qualified Data.List as List (find,inits,tails,findIndex,zip)
 import Data.Maybe (fromJust)
-import qualified Data.Text.Lazy as LT (append,pack,unpack)
+import qualified Data.Text.Lazy as LT (append,pack,unpack,toStrict)
 import Data.Char (chr)
 
 
@@ -42,19 +42,23 @@ articlesFilterTags = [
 
 crawlingFilterTags :: [(TagName,[Attr])]
 crawlingFilterTags = [
-  ("li", [Attr "class" "p-list-item  "]),
-  ("li", [Attr "class" "p-list-item p-list-item--small-thumb "])]
+  -- ("h3" , [Attr "class" "c-list-title"])
+    ("li", [Attr "class" "p-list-item  "]),
+    ("li", [Attr "class" "p-list-item p-list-item--small-thumb "])
+   ]
 
 filterTokens :: [(TagName,[Attr])] -> [Token] -> [Token]
-filterTokens tagsTuples tokens = concat nestedList where
+filterTokens tagsTuples tokens = concat nestedList  where
                            nestedList = filterFunction <*> (pure tokens) 
                            filterFunction = (map (uncurry $ filterToken) tagsTuples)
 
--- tratar erro aqui, caso a tag nao seja encontrada o programa irá retornar uma seção 
--- com a função fromJust
 filterToken :: TagName -> [Attr] -> [Token] -> [Token]
-filterToken tagname attr tokens = fromJust (List.find (\x -> mapping x == 0) withoutHead) where
+filterToken tagname attr tokens = case (List.findIndex (\x -> mapping x == 0) withoutHead) of
+                      Just x -> (withoutHead !! x) ++ (filterToken tagname attr $ tokenTails !! x)
+                      Nothing -> [] 
+                      where
                       mapping   = (foldr (countToken tagname) 0)
+                      tokenTails = drop 1 $ List.tails dropped
                       withoutHead = drop 1 $ List.inits dropped
                       dropped    = dropWhile ((/=) beginToken) tokens 
                       beginToken = TagOpen tagname attr
@@ -84,30 +88,10 @@ main = do
         let filteredTokens = (responseToFilteredTokens crawlingFilterTags response)
         let crawlingLinks = getLinksFromTokens filteredTokens
         mapM_ TIO.putStrLn crawlingLinks
-        -- IO.putStrLn (renderTokens filteredTokens)
-        let responses = get <$> (map T.unpack crawlingLinks)
-        let scrapped = map (fmap (responseToFilteredTokens articlesFilterTags )) responses
-        let rendered = map (fmap (renderTokens)) scrapped
-        IO.putStrLn <$> (head rendered)
-        mapM_ (fmap IO.putStrLn) rendered
+        IO.putStrLn (renderTokens filteredTokens)
+        responses <- (sequence) (get <$> (map T.unpack crawlingLinks))
+        let scrapped = map (renderTokens . responseToFilteredTokens articlesFilterTags ) responses
+ --       L.writeFile (T.unpack . head $ crawlingLinks) "fdsajk"
+        --let rendered = map (renderTokens) scrapped
+        mapM_ (L.appendFile "yomiuri.html") (map LTE.encodeUtf8 scrapped)
         return ()
-        -- mapM_ IO.putStrLn responses
-        --let url = "http://research.microsoft.com/en-us/people/simonpj/"
-        -- let url = "https://www.yomiuri.co.jp/national/20200128-OYT1T50285/"
-        --let url = "https://www.yomiuri.co.jp/world/20200129-OYT1T50107/"
-        --let url = "https://www.yomiuri.co.jp/hobby/atcars/impression/20200123-OYT8T50020/"
-        -- response <- get url
-        -- let body = response ^. responseBody :: L.ByteString
-        -- let tokens = parseTokensLazy decodedBody where
-        --              decodedBody = LTE.decodeUtf8 body
-        -- let parsedHtml = renderTokens article where 
-        --               article = filterTokens articlesFilterTags tokens 
-        -- let unpacked = LT.unpack $ parsedHtml 
-        -- --IO.writeFile "article3.html" parsedHtml
-        -- putStrLn unpacked
-
--- stringToAttr :: String -> String -> [Attr]
--- stringToAttr attr values = [Attr (T.pack attr) (T.pack values)]
-
--- stringToTagName :: String -> TagName
--- stringToTagName str = T.pack str
